20584e88ec9b62ea67c31854c4b11f2833524d96
shua1090 <shynn.lawrence@gmail.com>
2021-10-11 19:49:30 -0700

    getMovieRatingsThing(TestMethods.py) added

--- Model/TestMethods.py
+++ Model/TestMethods.py
@@ -1,9 +1,51 @@
 import math
 from random import randint
 
+import pandas as pd
+import numpy as np
+
 GuessTooLarge = 1
 CorrectGuess = 0
 GuessTooSmall = -1
+
+'''
+Return both a list of the least rated and most rated movies
+'''
+
+
+def getMovieRatingsThings() -> [list, list]:
+    df_ratings: pd.DataFrame = pd.read_csv('ratings.dat', sep="::",
+                                           names=['user_id', 'movie_id', 'rating', 'timestamp']).drop('timestamp',
+                                                                                                      1).pivot(
+        index='movie_id',
+        columns='user_id',
+        values='rating'
+    )
+    df_movies: pd.DataFrame = pd.read_csv('movies.dat', sep="::", names=['movie_id', 'title', 'genre'],
+                                          encoding='windows-1252')
+    df_movie_dict = dict(zip(df_movies.movie_id, df_movies.title))
+
+    df_ratings['null_count'] = df_ratings.isnull().sum(axis=1)
+    df_lowest_rating = df_ratings.sort_values('null_count', ascending=False).drop('null_count', axis=1)
+    df_highest_rating = df_ratings.sort_values('null_count', ascending=True).drop('null_count', axis=1)
+    listOfLowestRatedMovies = []
+    listOfHighestRatedMovies = []
+
+    j = 0
+    for i in df_lowest_rating.iterrows():
+        listOfLowestRatedMovies.append(df_movie_dict[i[0]])
+        j += 1
+        if (j > 100):
+            break
+
+    j = 0
+    for i in df_highest_rating.iterrows():
+        listOfHighestRatedMovies.append(df_movie_dict[i[0]])
+        j += 1
+        if (j > 100):
+            break
+
+        return listOfLowestRatedMovies, listOfHighestRatedMovies
 
 
 class NumberGuessing:
@@ -37,3 +79,7 @@ class NumberGuessing:
     def reset(self, value_range=(0, 100)):
         self.num = randint(*value_range)
         self.tries = int(math.log2(value_range[-1] - value_range[0])) + 1
+
+
+if __name__ == "__main__":
+    getMovieRatingsThings()
