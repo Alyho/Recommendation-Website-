19f51b87e88daa6e75ab78bc6f439479b555881e
shua1090 <shynn.lawrence@gmail.com>
2021-10-14 21:25:52 -0700

    Code couldn't find .dats, fixed maybe

--- Model/TestMethods.py
+++ Model/TestMethods.py
@@ -1,4 +1,6 @@
 import math
+import os.path
+import pathlib
 from random import randint
 
 import pandas as pd
@@ -14,14 +16,15 @@ Return both a list of the least rated and most rated movies
 
 
 def getMovieRatingsThings() -> [list, list]:
-    df_ratings: pd.DataFrame = pd.read_csv('Model/ratings.dat', sep="::",
+    prefix = pathlib.Path(__file__).parent.resolve().__str__()
+    df_ratings: pd.DataFrame = pd.read_csv(  prefix+'/ratings.dat', sep="::",
                                            names=['user_id', 'movie_id', 'rating', 'timestamp']).drop('timestamp',
                                                                                                       1).pivot(
         index='movie_id',
         columns='user_id',
         values='rating'
     )
-    df_movies: pd.DataFrame = pd.read_csv('Model/movies.dat', sep="::", names=['movie_id', 'title', 'genre'],
+    df_movies: pd.DataFrame = pd.read_csv(prefix+'/movies.dat', sep="::", names=['movie_id', 'title', 'genre'],
                                           encoding='windows-1252')
     df_movie_dict = dict(zip(df_movies.movie_id, df_movies.title))
 
